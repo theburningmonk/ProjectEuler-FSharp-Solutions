c377690d591202e83fefd2b748a1e38f4f64c3d4
theburningmonk <theburningmonk@gmail.com>
2013-07-06 13:43:42 +0100

    - added solution 65

---
+++ ProjectEulerSolutions/ProjectEulerSolutions/Problem065.fsx
@@ -0,0 +1,11 @@
+﻿let ``convergents of √2`` = seq { for i in 2I..2I..2000I do yield! [1I; i; 1I] }
+ 
+let answer = 
+    let numerator = 
+        ``convergents of √2``
+        |> Seq.skip 1
+        |> Seq.take 98
+        |> Seq.fold (fun (``n-1``, n) i -> (n, ``n-1`` + n * i)) (2I, 3I)
+        |> snd
+    
+    string numerator |> Seq.map (string >> int) |> Seq.sum
