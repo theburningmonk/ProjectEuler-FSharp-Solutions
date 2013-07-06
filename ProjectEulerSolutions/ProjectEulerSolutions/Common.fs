7c5619d967a819803fc5e86e3d69aeda95fa0ee9
theburningmonk <theburningmonk@gmail.com>
2013-07-06 13:37:46 +0100

    - moved prime sieve, etc into a common module - moved solution to vs12 - added solution for problem 60 - added gitignore

---
+++ ProjectEulerSolutions/ProjectEulerSolutions/Common.fs
@@ -0,0 +1,57 @@
+﻿module Common
+
+open System.Collections
+open System.Collections.Generic
+
+// generate prime numbers up to the specified max
+let genPrimes max =
+    // define a cache for holding records of which number is a prime
+    let cache = new BitArray(max+1, true)
+ 
+    // using prime sieve to fill out the cache
+    [| 2..max |]
+    |> Array.iter (fun n ->
+        if cache.[n] then
+            [| 2..max / n |] 
+            |> Array.iter (fun m -> cache.[n * m] <- false))
+ 
+    // build a set of prime numbers using the cache
+    [| 2..max |] 
+    |> Array.filter (fun n -> cache.[n])
+
+let rec distribute e = function
+    | [] -> [[e]]
+    | x::xs' as xs -> (e::xs)::[for xs in distribute e xs' -> x::xs]
+ 
+// generate permutations of a list
+let rec permute = function
+    | [] -> [[]]
+    | e::xs -> List.collect (distribute e) (permute xs)
+ 
+// generate combinations of n elements from the list l
+let rec comb n (l : 'a list) =
+    match n, l with
+    | 0, _ -> seq { yield [] }
+    | _, [] -> Seq.empty<'a list>
+    | k, (x::xs) -> 
+        seq {
+            yield! Seq.map ((@) [x]) (comb (k-1) xs)
+            yield! comb k xs
+        }
+
+/// Applies memoization to the supplied function f
+let memoize (f : 'a -> 'b) =
+    let cache = new Dictionary<'a, 'b>()
+
+    let memoizedFunc (input : 'a) =
+        // check if there is a cached result for this input
+        match cache.TryGetValue(input) with
+        | true, x   -> x
+        | false, _  ->
+            // evaluate and add result to cache
+            let result = f input
+            cache.Add(input, result)
+            result
+
+    // return the memoized version of f
+    memoizedFunc
---
+++ ProjectEulerSolutions/ProjectEulerSolutions/Problem060.fsx
@@ -0,0 +1,32 @@
+﻿#load "Common.fs"
+
+open Common
+
+let max = 10000
+let primes = genPrimes max |> Array.toList
+
+let isPrime' n = 
+    if n % 2 = 0 then false 
+    elif n % 3 = 0 then false
+    else let sqrtn = float n |> sqrt |> ceil |> int
+         seq { 5..2..sqrtn } |> Seq.exists (fun x -> n % x = 0) |> not
+let isPrime = memoize isPrime'
+
+let concatsToPrime' (a, b) = 
+    let f a b = a * pown 10 (int (log10 (float b) + 1.0)) + b
+    isPrime(f a b) && isPrime(f b a)
+let concatsToPrime = memoize concatsToPrime'
+
+let rec comb n (l : int list) acc =
+    match n, l with
+    | 0, _  -> seq { yield [] }
+    | _, [] -> Seq.empty<int list>
+    | k, (x::xs) -> 
+        seq {
+            if acc |> List.forall (fun y -> concatsToPrime(x, y)) 
+            then yield! Seq.map ((@) [x]) (comb (k-1) xs (x::acc))
+
+            yield! comb k xs acc
+        }
+
+let combos = comb 5 primes [] |> Seq.map List.sum |> Seq.min
