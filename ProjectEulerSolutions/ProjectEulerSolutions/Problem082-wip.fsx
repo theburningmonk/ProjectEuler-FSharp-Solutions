7edc00916e09904a34c9d410dfa0fb45790c7d2c
theburningmonk <theburningmonk@gmail.com>
2014-10-18 03:59:06 +0100

    - added WIP for 82 and 111

---
+++ ProjectEulerSolutions/ProjectEulerSolutions/Problem082-wip.fsx
@@ -0,0 +1,28 @@
+﻿open System
+open System.IO
+
+let dimension = 80
+
+// load the original matrix
+let matrix = array2D (File.ReadAllLines(@"c:\temp\matrix.txt") 
+                      |> Array.map (fun l -> l.Split(',') |> Array.map int32))
+
+// init the shortes sum matrix
+let sumMatrix = Array2D.init dimension dimension (fun i j -> if j = 0 then matrix.[i, j] else 0)
+
+let fromTop i j    = sumMatrix.[i - 1, j] + matrix.[i, j]
+let fromBelow i j  = sumMatrix.[i + 1, j] + matrix.[i, j]
+let fromLeft i j   = sumMatrix.[i, j - 1] + matrix.[i, j]
+
+// for each position (i, j) find the shortest sum leading to this point by moving down or
+// right from the previous point in the path
+for i = 0 to dimension - 1 do
+    for j = 0 to dimension - 1 do
+        match (i, j) with
+        | (_, 0) -> ()
+        | (0, _) -> sumMatrix.[i, j] <- min (fromLeft i j) (fromBelow i j)
+        | (_, _) when i < (dimension - 1)
+                 -> sumMatrix.[i, j] <- min (fromLeft i j) (min (fromTop i j) (fromBelow i j))
+        | (_, _) -> sumMatrix.[i, j] <- min (fromLeft i j) (fromTop i j)
+        
+let answer = [| 0..dimension-1 |] |> Array.map (fun i -> sumMatrix.[i, dimension-1]) |> Array.min
---
+++ ProjectEulerSolutions/ProjectEulerSolutions/Problem111-wip.fsx
@@ -0,0 +1,14 @@
+﻿#load "Common.fs"
+
+open Common
+
+let isPrime' n = 
+    if n % 2L = 0L then false 
+    elif n % 3L = 0L then false
+    else let sqrtn = float n |> sqrt |> ceil |> int64
+         seq { 5L..2L..sqrtn } |> Seq.exists (fun x -> n % x = 0L) |> not
+let isPrime = memoize isPrime'
+
+let M n d =
+    let gen () =
+        
