d0670f1fad6182e6f3556d4fa7073a03e2388373
theburningmonk <theburningmonk@gmail.com>
2014-10-18 15:52:02 +0100

    - refactored 64

--- ProjectEulerSolutions/ProjectEulerSolutions/Problem064.fsx
+++ ProjectEulerSolutions/ProjectEulerSolutions/Problem064.fsx
@@ -3,16 +3,11 @@
 let expand n =
     let m0, d0, a0 = 0, 1, float n |> sqrt |> int
 
-    let (|Repeat|_|) (acc : int list) =
-        if acc.Length = 0 || acc.Length % 2 <> 0 then None
-        elif Seq.head acc <> 2 * a0 then None
-        else let half = acc.Length / 2
-             let fst, snd = acc |> Seq.take half |> Seq.toArray, 
-                            acc |> Seq.skip half |> Seq.toArray
-             if fst = snd then Some fst else None
+    let (|Repeat|_|) (acc : int list) = 
+        if acc.Length > 0 && Seq.head acc = 2 * a0 then Some acc else None
 
     let rec loop m d a = function
-        | Repeat arr -> arr.Length
+        | Repeat lst -> lst.Length
         | acc ->
             let m' = d * a - m
             let d' = (n - (m' * m')) / d
