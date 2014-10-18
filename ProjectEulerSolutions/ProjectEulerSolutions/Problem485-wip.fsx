2f488397fb0f8c7bf2fa09ded6e2b2cd04d205f0
theburningmonk <theburningmonk@gmail.com>
2014-10-18 16:53:03 +0100

    - added WIP on 485

---
+++ ProjectEulerSolutions/ProjectEulerSolutions/Problem485-wip.fsx
@@ -0,0 +1,31 @@
+﻿#load "Common.fs"
+
+open Common
+
+#time
+
+let primes = genPrimes 1000000 |> Set.ofArray
+
+let d' n = 
+    let rec loop n acc =
+        if primes.Contains n then n::acc
+        else 
+            match primes |> Seq.tryFind (fun div -> n % div = 0) with
+            | Some div -> loop (n / div) (div::acc)
+            | _ -> n::acc
+
+    let primeDivisors = loop n []
+    primeDivisors 
+    |> Seq.groupBy id 
+    |> Seq.map (fun (_, gr) -> (gr |> Seq.length) + 1) 
+    |> Seq.fold (*) 1
+    |> int64
+
+let d = memoize d'
+
+let M'(n, k) = { n..n+k-1 } |> Seq.maxBy d
+let M        = memoize M'
+let S(u, k)  = { 1..u-k+1 } |> Seq.sumBy (fun n -> M(n, k) |> int64)
+
+if 17176L <> S(1000, 10) then failwith "test case failed"
+let answer = S(1000000000, 1000000)
