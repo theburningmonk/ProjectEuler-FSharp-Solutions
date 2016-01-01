f41c10798a922119a4cc9dc40b99b94cb64fd331
Yan Cui <theburningmonk@gmail.com>
2016-01-01 23:47:54 +0000

    - added wip for 357

---
+++ ProjectEulerSolutions/ProjectEulerSolutions/Problem357-wip.fsx
@@ -0,0 +1,18 @@
+﻿#load "Common.fs"
+
+open Common
+open Checked
+
+#time
+
+let sqrt   = sqrt >> int
+let primes = genPrimes (sqrt 100000000.0) |> Set.ofArray
+
+let pred n =
+    let max = sqrt (float n)
+
+    { 1..max }
+    |> Seq.filter (fun m -> n % m = 0)
+    |> Seq.forall (fun m -> primes.Contains <| m + n/m)
+   
+let answer = { 1..100000000 } |> Seq.filter pred |> Seq.sum
