e45e425b34798b7f4e0af0c625bfe48ec909b99b
Yan Cui <theburningmonk@gmail.com>
2016-01-01 21:58:35 +0000

    - solved 82

--- ProjectEulerSolutions/ProjectEulerSolutions/Problem082-wip.fsx
+++ ProjectEulerSolutions/ProjectEulerSolutions/Problem082-wip.fsx
@@ -13,41 +13,44 @@ let dim = Array2D.length1 matrix
 // init the shortes sum matrix
 let sumMatrix = 
     (fun row col -> 
-        if col = 0 then matrix.[row, col] else 0)
+        if col = 0 
+        then matrix.[row, col] 
+        else matrix.[row, 0..col] |> Array.sum)
     |> Array2D.init dim dim 
 
-let fromTopDown row col = 
-    sumMatrix.[row-1, col-1] + 
-    matrix.[row-1, col] + 
-    matrix.[row, col]
-let fromBottomUp row col = 
-    sumMatrix.[row+1, col-1] + 
-    matrix.[row+1, col] +
-    matrix.[row, col]
-let fromLeft row col = 
-    sumMatrix.[row, col-1] + 
-    matrix.[row, col]
+let fromTop row col =
+    sumMatrix.[row-1, col] + matrix.[row, col]
+let fromBelow row col =
+    sumMatrix.[row+1, col] + matrix.[row, col]
+let fromLeft row col =
+    sumMatrix.[row, col-1] + matrix.[row, col]
 
-// for each position (row, col) find the shortest sum leading to 
-// this point by moving down, up or right from the previous 
-// point in the path
+let rec optimize col =
+    let calNewMin row =
+        if row = 0 then
+            min (fromLeft row col) (fromBelow row col)
+        elif row = dim-1 then
+            min (fromLeft row col) (fromTop row col)
+        else 
+            fromLeft row col
+            |> min (fromTop row col)
+            |> min (fromBelow row col)
+
+    let changes =
+        { 0..dim-1 }
+        |> Seq.sumBy (fun row ->
+            let current = sumMatrix.[row, col]
+            let newMin  = calNewMin row
+            if newMin < current then
+                sumMatrix.[row, col] <- newMin
+                1
+            else 0)
+
+    if changes > 0 then optimize col
+
 for col = 1 to dim-1 do
-    for row = 0 to dim-1 do
-        let left = fromLeft row col
+    optimize col
 
-        match (row, col) with
-        | (0, _) -> 
-            let bottomUp = fromBottomUp row col
-            sumMatrix.[row, col] <- min left bottomUp
-        | (_, _) when row = dim-1 -> 
-            let topDown = fromTopDown row col
-            sumMatrix.[row, col] <- min left topDown
-        | (_, _) -> 
-            let topDown  = fromTopDown row col
-            let bottomUp = fromBottomUp row col
-            let minSum   = min left <| min topDown bottomUp
-            sumMatrix.[row, col] <- minSum
-        
 let answer = 
     [| 0..dim-1 |] 
     |> Array.map (fun row -> sumMatrix.[row, dim-1]) 
